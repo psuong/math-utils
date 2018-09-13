e998e44d4dc481f27558956a225da66416418434
Porrith Suong <psuong95@gmail.com>
2018-09-12 23:43:12 -0400

    Adds tests to assert rotation between in front and behind.

--- Tests/Trigonometry/RotationTests.cs
+++ Tests/Trigonometry/RotationTests.cs
@@ -1,15 +1,32 @@
 ﻿using Unity.Mathematics;
-using UnityEngine.TestTools;
-using MathUtils;
+using UnityEngine;
 using NUnit.Framework;
 
 namespace MathUtils.Tests {
 
+    /// <summary>
+    /// Tests simple rotations about an axis.
+    /// </summary>
     public class RotationTests {
 
         [Test]
-        public void RotationTestsSimplePasses() {
+        public void APointRotatedAlongZWithZeroDegreesIsInFront() {
             var pivot = new float3(0, 0, 0);
+            var rotatedPoint = RotationUtils.RotateAbout(pivot, 0f, 1f);
+
+            Debug.LogFormat("<color=#00ff00ff>Pivot: {0}, Rotated Point: {1}</color>", pivot, rotatedPoint);
+
+            Assert.Less(pivot.z, rotatedPoint.z, "The rotated point is not in front of the pivot!");
+        }
+
+        [Test]
+        public void APointRotatedAlongZWith180DegreesIsInBack() {
+            var pivot = new float3(0, 0, 1f);
+            var rotatedPoint = RotationUtils.RotateAbout(pivot, 180f, 1f);
+
+            Debug.LogFormat("<color=#00ff00ff>Pivot: {0}, Rotated Point: {1}</color>", pivot, rotatedPoint);
+
+            Assert.Greater(pivot.z, rotatedPoint.z, "The rotated point is not behind the pivot!");
         }
     }
 }
