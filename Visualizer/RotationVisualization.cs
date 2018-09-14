960db4ce6e26e828c29c8383aa0c71b07818e7a9
Porrith Suong <psuong95@gmail.com>
2018-09-14 14:36:05 -0400

    Makes the visualization more comprehensive by drawing directions.

--- Visualizer/RotationVisualization.cs
+++ Visualizer/RotationVisualization.cs
@@ -8,13 +8,18 @@ namespace MathUtils.Visualizer {
     /// </summary>
     public class RotationVisualization : MonoBehaviour {
 
+        [Tooltip("How wide is the circle?")]
         public float radius = 0.1f;
+        [Tooltip("What are the points to pivot on and how wide is the rotation?")]
         public float3 point, pivot, angle;
+        [Tooltip("What are the colors of these visualized points?")]
         public Color pointColor = Color.green, pivotColor = Color.red, rotatedPointColor = Color.cyan;
 
         private void OnDrawGizmos() {
             Gizmos.color = pointColor;
             Gizmos.DrawSphere(point, radius);
+
+            Gizmos.DrawLine(pivot, point);
 
             Gizmos.color = pivotColor;
             Gizmos.DrawSphere(pivot, radius);
@@ -22,6 +27,7 @@ namespace MathUtils.Visualizer {
             Gizmos.color = rotatedPointColor;
             var rotatedPoint = RotationUtils.RotateAbout(pivot, point, angle);
             Gizmos.DrawSphere(rotatedPoint, radius);
+            Gizmos.DrawLine(pivot, rotatedPoint);
         }
     }
 }
